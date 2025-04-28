1bc520bbc4beacc74ee763485a9da1447edbee08
PolixTer <clevertonruppenthal1@gmail.com>
2025-04-28 22:39:05 +0100

    Clone and test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="min-h-screen max-h-full bg-zinc-950">
+    <div className="min-h-screen max-h-full bg-red-500">
       <div className="font-bold text-white">Testing</div>
     </div>
   );
